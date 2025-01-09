7f582f2f2e0e68081542a34661a8a16b72fb49b5
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-09 13:53:15 +0600

    delete option done & update option working

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 
 const App = () => {
   let [inputValue, setInputValue] = useState('');
-  let [output, setOutput] = useState('');
+  let [output, setOutput] = useState([]);
+  let [edit, setedit]=useState('')
 
   let handleInput= (e)=>{
     setInputValue(e.target.value)
   }
 
   let handleSubmit=()=>{
-    setOutput(inputValue)
+    setOutput([...output, inputValue])
+  }
+
+  let handleDelete = (index)=>{
+    setOutput(output.filter((_, i) => i !== index))
+  }
+
+  let handleEdit =(index)=>{
+    
   }
   return (
     <div className='container'>
@@ -19,13 +28,22 @@ const App = () => {
         <input onChange={handleInput} type="text" placeholder='Enter Task...' className='w-[700px] px-3 py-3 text-[16px] font-medium border' />
         <button onClick={handleSubmit} className='px-3 py-3 text-[16px] font-medium border bg-teal-300' >Submit</button>
         </div>
-        {output && 
-        (
+        {output.length>0 && (
           <div>
-          <h2>{output}</h2>
+            <h2 className=' mt-5 mx-auto text-center text-[18px] font-semibold'>Task Manager</h2>
+            {output.map ((task, index)=>(
+          <div key={index}>
+            <ul><li className='flex justify-between px-[20px]'>{task}
+              <div>
+                <button onClick={()=>handleEdit(index)}>Edit</button>
+                <button onClick={()=>handleDelete(index)} className='text-red-500'>X</button>
+              </div>
+              </li></ul>
           </div>
-        )
-      }
+        ))}
+          </div>
+        )}
+        
       </div>
       
     </div>
